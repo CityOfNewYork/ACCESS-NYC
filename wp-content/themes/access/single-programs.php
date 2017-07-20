4fdd5cdf526f0451f2227dfbc9bf827ecb7c4c89
devowhippit <devonhirth@gmail.com>
2017-07-20 09:49:18 -0400

    Removing html entities from parameters, rebuilding share url query manually, using post links instead of browser url as base for sharing url

--- wp-content/themes/access/single-programs.php
+++ wp-content/themes/access/single-programs.php
@@ -7,10 +7,12 @@ $context = Timber::get_context();
 
 // Gets the url parameter on the page for navigating each section.
 if (isset($_GET['step'])) {
-  $context['step'] = $_GET['step'];
+  $context['step'] = htmlspecialchars($_GET['step']);
 } else {
   $context['step'] = '';
 }
+
+$query = ($context['step'] !== '') ? '?step='.$context['step'] : '';
 
 $post = Timber::get_post();
 $templates = array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' );
@@ -18,7 +20,7 @@ $context['post'] = $post;
 
 // Share by email/sms fields.
 $context['shareAction'] = admin_url( 'admin-ajax.php' );
-$context['shareUrl'] = \SMNYC\get_current_url();
+$context['shareUrl'] = $post->link.$query;
 $context['shareHash'] = \SMNYC\hash($context['shareUrl']);
 
 Timber::render( $templates, $context );
