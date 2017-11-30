6232f33a8cfb8cee60435c6c748a7ed6e2786ef9
kimberly peng <kimmypeng@gmail.com>
2017-11-30 13:16:48 -0500

    ac-225 adding filter to clean up the mapped wp fields in gc template build

--- wp-content/themes/access/functions.php
+++ wp-content/themes/access/functions.php
@@ -579,6 +579,23 @@ add_action('wp', 'trigger_gmaps', 1);
 // end of trigger_gmaps
 
 
+// GatherContent - Mapped WordPress Field meta_keys edit
+add_filter( 'gathercontent_importer_custom_field_keys', function( $meta_keys ) {
+  // empty array that will contain the unique meta keys for the mapped fields
+  $new_meta_keys = array();
+
+  // Creates a new array of meta_keys that are not prefixed with underscore
+  foreach ($meta_keys as $key=>$value) {
+    if (substr($value, 0, 1) != '_') {
+      $new_meta_keys[$key]=$value;
+    }
+  }
+  // return the new array 
+  return $new_meta_keys;
+} );
+// end of GatherContent - Mapped WordPress Field meta_keys edit
+
+
 function validate_params($namespace, $subject) {
   $patterns = array(
     'programs'=> '/^[A-Z0-9,]*$/',
