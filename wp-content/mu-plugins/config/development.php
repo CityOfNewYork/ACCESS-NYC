cb19713cff1562519fd994b5840f985aa1e98ef4
devowhippit <devonhirth@gmail.com>
2018-10-11 15:06:02 -0400

    Deactivate rollbar locally

--- wp-content/mu-plugins/config/development.php
+++ wp-content/mu-plugins/config/development.php
@@ -7,3 +7,4 @@
 // Disable plugins
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
 deactivate_plugins('google-authenticator/google-authenticator.php');
+deactivate_plugins('rollbar/rollbar-php-wordpress.php');
