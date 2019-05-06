ecf7bc110efc9b624de8197bf03b4e6ff33ea31d
devowhippit <devonhirth@gmail.com>
2019-05-06 16:45:05 -0400

    Add includes support for screener. Update screener selector

--- wp-content/themes/access/src/js/screener.js
+++ wp-content/themes/access/src/js/screener.js
@@ -1,13 +1,14 @@
 /* eslint-env browser */
-import jQuery from 'jquery';
+// Core-js polyfills.
+// Core-js is made available as a dependency of @babel/preset-env
+import 'core-js/features/array/includes';
+
 import Screener from 'modules/screener';
 
-(function(window, $) {
+(function() {
   'use strict';
 
   // Initialize eligibility screener.
-  $(`.${Screener.CssClass.FORM}`).each((i, el) => {
-    const screener = new Screener(el);
-    screener.init();
-  });
-})(window, jQuery);
+  let el = document.querySelector(Screener.Selectors.FORM);
+  if (el) new Screener(el).init();
+})();
