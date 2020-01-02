05dbeb3aefca22d644c8463d8557ca6d9239cb35
devowhippit <devonhirth@gmail.com>
2020-01-02 15:47:45 -0500

    Disable phpcs for config file

--- wp-content/mu-plugins/@config.php
+++ wp-content/mu-plugins/@config.php
@@ -1,12 +1,10 @@
 <?php
 
-// phpcs:disable
-/**
- * Plugin Name: NYCO WordPress Config
- * Description: Composer Managed. This "Must Use" WordPress Plugin sets environment variables for a WordPress installation through a YAML configuration file and autoloads environment specific scripts.
- * Author: NYC Opportunity
- */
-// phpcs:enable
+/*
+Plugin Name: NYCO WordPress Config
+Description: Composer Managed. This "Must Use" WordPress Plugin sets environment variables for a WordPress installation through a YAML configuration file and autoloads environment specific scripts.
+Author: NYC Opportunity
+*/
 
 if (file_exists(WPMU_PLUGIN_DIR . '/wp-config/Config.php')) {
   require_once WPMU_PLUGIN_DIR . '/wp-config/Config.php';
