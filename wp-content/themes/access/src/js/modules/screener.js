239be38a39f668106fb743205139d6c8d35a800b
devowhippit <devonhirth@gmail.com>
2020-01-22 15:48:08 -0500

    Disable HOH patch

--- wp-content/themes/access/src/js/modules/screener.js
+++ wp-content/themes/access/src/js/modules/screener.js
@@ -113,11 +113,11 @@ class Screener {
       let input = step.find('input[name="Person[0].headOfHousehold"]:checked');
 
       // Reset HOH to be element at index 0 and remove current HOH from array.
-      this._people.forEach((member, index) => {
-        if(member._attrs.headOfHousehold === true && index !== 0) {
-          this._people.splice(index, 1);
-        }
-      });
+      // this._people.forEach((member, index) => {
+      //   if(member._attrs.headOfHousehold === true && index !== 0) {
+      //     this._people.splice(index, 1);
+      //   }
+      // });
 
       this._people[0].set({
         headOfHousehold: Screener.getTypedVal(input)
@@ -1218,10 +1218,10 @@ class Screener {
     $('#recap-body').html(renderedTemplate);
 
     // Reset personIndex to zero on edit the head of household.
-    let editPersonButton = document.getElementById('recap-edit-person');
-    editPersonButton.addEventListener('click', function resetPersonIndex() {
-      this._$steps.data('personIndex', 0);
-    }.bind(this));
+    // let editPersonButton = document.getElementById('recap-edit-person');
+    // editPersonButton.addEventListener('click', function resetPersonIndex() {
+    //   this._$steps.data('personIndex', 0);
+    // }.bind(this));
 
     return this;
   }
