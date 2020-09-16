221678bce23f0b6cf2e266f9294156a171d88fb4
devowhippit <devonhirth@gmail.com>
2020-09-16 16:47:31 -0400

    Add model for single programs toggle

--- wp-content/themes/access/src/js/single-programs.js
+++ wp-content/themes/access/src/js/single-programs.js
@@ -1,5 +1,10 @@
 /* eslint-env browser */
-import ProgramsDetail from 'modules/programs-detail';
+// Core-js polyfills.
+// Core-js is made available as a dependency of @babel/preset-env
+import 'core-js/features/url-search-params';
+
+// Patterns Framework
+import Toggle from 'utilities/toggle/toggle';
 
 /**
 * Programs Detail
@@ -7,10 +12,20 @@ import ProgramsDetail from 'modules/programs-detail';
 (() => {
   'use strict';
 
-  (element => {
-    if (element) {
-      new ProgramsDetail();
+  const query = new URLSearchParams(window.location.search);
+
+  // if there is a query, and an existing section,
+  //   show appropriate section
+
+  new Toggle({
+    selector: '[data-js*="pager"]',
+    before: () => {
+      // hide all sections
+    },
+    after: () => {
+      // scroll to the top of section parent
+      // push state to history api
     }
-  })(document.querySelector(ProgramsDetail.selector));
+  });
 })();
 
