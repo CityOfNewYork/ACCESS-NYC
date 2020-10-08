1d9478046395980257e11ac64140cce6a1af2f93
devowhippit <devonhirth@gmail.com>
2020-10-08 12:31:33 -0400

    Update query vars, including a/b testing variant

--- wp-content/mu-plugins/query-vars.php
+++ wp-content/mu-plugins/query-vars.php
@@ -12,6 +12,21 @@ add_filter('query_vars', function($vars) {
   $vars[] = 'program_cat'; // Used in Programs Archive
   $vars[] = 'pop_served'; // Used in Programs Single
   $vars[] = 'page_type'; // Used in Programs Single
+  $vars[] = 'print';
+
+  /**
+   * Please note, custom query parameters should use the prefix
+   * to prevent further conflicts with the WordPress Query
+   *
+   * @author NYC Opportunity
+   */
+
+  $prefix = 'anyc_';
+
+  $vars[] = $prefix . 'categories'; // Program Categories
+  $vars[] = $prefix . 'served'; // Population Served
+  $vars[] = $prefix . 'print'; // Print Views
+  $vars[] = $prefix . 'v'; // A/B Testing Variants
 
   return $vars;
 });
