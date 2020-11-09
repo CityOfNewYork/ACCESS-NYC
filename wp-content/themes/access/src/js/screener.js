93b71366173bc9186e465016c1eb329392571da5
Aguilar <stevenaguilarcs@gmail.com>
2020-11-09 15:47:26 -0500

    Create module script for Share Form and Disclaimer

--- wp-content/themes/access/src/js/screener.js
+++ wp-content/themes/access/src/js/screener.js
@@ -2,14 +2,10 @@
 
 // Core Modules
 import Screener from 'modules/screener';
-
-// ACCESS Patterns
-import ShareForm from 'components/share-form/share-form';
-import Disclaimer from 'components/disclaimer/disclaimer';
+import ShareFormDisclaimer from 'modules/share-form-disclaimer';
 
 // Patterns Framework
 import Tooltips from 'utilities/tooltips/tooltips';
-import localize from 'utilities/localize/localize';
 
 (function() {
   'use strict';
@@ -24,38 +20,5 @@ import localize from 'utilities/localize/localize';
   (elements => {
     elements.forEach(element => new Tooltips(element));
   })(document.querySelectorAll(Tooltips.selector));
-
-  /**
-   * Initialize the Share Form and Disclaimer
-   */
-  (elements => {
-    elements.forEach(element => {
-      let shareForm = new ShareForm(element);
-      let strings = Object.fromEntries([
-        'SHARE_FORM_SERVER',
-        'SHARE_FORM_SERVER_TEL_INVALID',
-        'SHARE_FORM_VALID_REQUIRED',
-        'SHARE_FORM_VALID_EMAIL_INVALID',
-        'SHARE_FORM_VALID_TEL_INVALID'
-      ].map(i => [
-        i.replace('SHARE_FORM_', ''),
-        localize(i)
-      ]));
-
-      shareForm.strings = strings;
-      shareForm.form.strings = strings;
-
-      shareForm.sent = instance => {
-        let key = instance.type.charAt(0).toUpperCase() +
-          instance.type.slice(1);
-
-        Track.event(key, [
-          {'DCS.dcsuri': `share/${instance.type}`}
-        ]);
-      };
-    });
-
-    new Disclaimer();
-  })(document.querySelectorAll(ShareForm.selector));
 
 })();
--- wp-content/themes/access/src/js/single-programs.js
+++ wp-content/themes/access/src/js/single-programs.js
@@ -1,13 +1,7 @@
 /* eslint-env browser */
 
 import StepByStep from 'modules/step-by-step';
-
-// ACCESS Patterns
-import ShareForm from 'components/share-form/share-form';
-import Disclaimer from 'components/disclaimer/disclaimer';
-
-// Patterns Framework
-import localize from 'utilities/localize/localize';
+import ShareFormDisclaimer from 'modules/share-form-disclaimer';
 
 (() => {
   'use strict';
@@ -19,37 +13,5 @@ import localize from 'utilities/localize/localize';
     if (element) new StepByStep(element);
   })(document.querySelector(StepByStep.selector));
 
-  /**
-   * Initialize the Share Form and Disclaimer
-   */
-  (elements => {
-    elements.forEach(element => {
-      let shareForm = new ShareForm(element);
-      let strings = Object.fromEntries([
-        'SHARE_FORM_SERVER',
-        'SHARE_FORM_SERVER_TEL_INVALID',
-        'SHARE_FORM_VALID_REQUIRED',
-        'SHARE_FORM_VALID_EMAIL_INVALID',
-        'SHARE_FORM_VALID_TEL_INVALID'
-      ].map(i => [
-        i.replace('SHARE_FORM_', ''),
-        localize(i)
-      ]));
-
-      shareForm.strings = strings;
-      shareForm.form.strings = strings;
-
-      shareForm.sent = instance => {
-        let key = instance.type.charAt(0).toUpperCase() +
-          instance.type.slice(1);
-
-        Track.event(key, [
-          {'DCS.dcsuri': `share/${instance.type}`}
-        ]);
-      };
-    });
-
-    new Disclaimer();
-  })(document.querySelectorAll(ShareForm.selector));
 })();
 
