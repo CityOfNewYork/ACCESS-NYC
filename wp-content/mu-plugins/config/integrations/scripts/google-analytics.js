1b81e8d9c1f50629116c05e0dffb54dd420d88c5
devowhippit <devonhirth@gmail.com>
2021-03-17 16:42:07 -0400

    Check for Window setting in Google Optimize script

--- wp-content/mu-plugins/config/integrations/scripts/google-analytics.js
+++ wp-content/mu-plugins/config/integrations/scripts/google-analytics.js
@@ -1,3 +1,7 @@
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', '{{ GOOGLE_ANALYTICS }}', { 'transport_type': 'beacon'});
+if (window.hasOwnProperty('GOOGLE_OPTIMIZE')) {
+  gtag('config', '{{ GOOGLE_ANALYTICS }}', {'optimize_id': '{{ GOOGLE_OPTIMIZE_ID }}', 'transport_type': 'beacon'});
+} else {
+  gtag('config', '{{ GOOGLE_ANALYTICS }}', {'transport_type': 'beacon'});
+}
--- wp-content/mu-plugins/config/integrations/scripts/google-optimize.js
+++ wp-content/mu-plugins/config/integrations/scripts/google-optimize.js
@@ -3,3 +3,5 @@
   h.end = i = function() {s.className = s.className.replace(RegExp(' ?' + y), '');};
   (a[n] = a[n] || []).hide = h; setTimeout(function() {i(); h.end = null}, c); h.timeout = c;
 })(window, document.documentElement, 'async-hide', 'dataLayer', 4000, {'{{ GOOGLE_OPTIMIZE_ID }}': true});
+
+window.GOOGLE_OPTIMIZE = true;
