83adea5b57d7ad6f4557075a74b769408a29305c
devowhippit <devonhirth@gmail.com>
2021-03-31 10:06:00 -0400

    v3.30.1

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInite075c4026d211701dae3f1417011784e
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
