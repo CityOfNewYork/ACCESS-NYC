218881df711d2e7bee2552593baaeb0ebe8f6bd7
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2023-03-02 09:48:50 -0500

    lint register-menus.php

--- wp-content/mu-plugins/register-menus.php
+++ wp-content/mu-plugins/register-menus.php
@@ -8,11 +8,11 @@
 add_action('init', function() {
   register_nav_menus(
     array(
-      'header-menu' => __( 'Header Primary Menu' ),
-      'get-help-now' => __( 'Footer Menu 1 [left]' ),
-      'for-caseworkers' => __( 'Footer Menu 2 [left]' ),
-      'programs' => __( 'Footer Menu 3 [center]' ),
-      'about-access-nyc' => __( 'Footer Menu 4 [right]' ),
+      'header-menu' => __('Header Primary Menu'),
+      'get-help-now' => __('Footer Menu 1 [left]'),
+      'for-caseworkers' => __('Footer Menu 2 [left]'),
+      'programs' => __('Footer Menu 3 [center]'),
+      'about-access-nyc' => __('Footer Menu 4 [right]'),
     )
   );
 });
