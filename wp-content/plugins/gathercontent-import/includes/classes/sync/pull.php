7bebe762343e966d9f913d3635b882f56e6f3165
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2024-05-23 15:00:05 -0400

    log choice checkbox value

--- wp-content/plugins/gathercontent-import/includes/classes/sync/pull.php
+++ wp-content/plugins/gathercontent-import/includes/classes/sync/pull.php
@@ -625,6 +625,7 @@ class Pull extends Base {
 		if ( 'text' === $this->element->type ) {
 			$terms = array_map( 'trim', explode( ',', sanitize_text_field( $this->element->value ) ) );
 		} elseif ( 'choice_checkbox' === $this->element->type ) {
+			error_log(print_r($this->element, true));
 			$terms = json_decode($this->element->value);
 		} else {
 			$terms = (array) $this->element->value;
