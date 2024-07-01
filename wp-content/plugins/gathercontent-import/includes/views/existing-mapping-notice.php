e447deb952ffe01a755d47aa4bbcf4ab174fbfd8
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2024-07-01 10:53:04 -0400

    remove gathercontent

--- wp-content/plugins/gathercontent-import/includes/views/existing-mapping-notice.php
+++
@@ -1,3 +0,0 @@
-<div class="notice notice-info is-dismissible">
-	<p><?php printf( __( '<strong>NOTE:</strong> There can be only one %1$s per project template. You are editing an existing mapping (ID: %2$d).', 'gathercontent-import' ), $this->get( 'name' ), $this->get( 'id' ) ); ?></p>
-</div>
--- wp-content/plugins/gathercontent-import/includes/views/tmpl-gc-select2-item.php
+++
@@ -1,9 +0,0 @@
-<span>
-	{{ data.text }}
-	<# if ( data.color ) { #>
-	<span class="gc-status-color <# if ( '#ffffff' === data.color ) { #> gc-status-color-white<# } #>" style="background-color:{{ data.color }};"></span>
-	<# } #>
-</span>
-<# if ( data.description ) { #>
-<div class="description">{{ data.description }}</div>
-<# } #>
