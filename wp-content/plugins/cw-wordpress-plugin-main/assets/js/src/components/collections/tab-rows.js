2784d18e56a4ab0c7646aee006f6cd41cbfd3085
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2024-07-31 10:39:23 -0400

    Revert "add content workflow plugin"
    This reverts commit 15fd68c07fdeaf2b132ec061db7cfd54797900ea.

--- wp-content/plugins/cw-wordpress-plugin-main/assets/js/src/components/collections/tab-rows.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (app) {
-	return app.collections.base.extend({
-		model: app.models.tabRow,
-
-		initialize: function (models, options) {
-			this.tab = options.tab;
-		}
-	});
-};
