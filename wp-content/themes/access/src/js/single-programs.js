3f2b532c250dc63bb1ed75f6d7c2a3f94b5d2317
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2025-01-16 19:11:26 -0500

    make only the nav on the program guide sticky and not the header, on mobile

--- wp-content/themes/access/src/js/single-programs.js
+++ wp-content/themes/access/src/js/single-programs.js
@@ -15,24 +15,23 @@ import 'modules/share-form';
         const sections = document.querySelectorAll("section");
         const sideLinks = document.querySelectorAll(".side-nav-link");
         const topLinks = document.querySelectorAll(".top-nav-link");
-        const header = document.querySelector("#program-guide-header");
+        const topNav = document.querySelector(".c-top-nav");
 
         // for top nav horizontal scrolling
         function centerActiveLinkIfScrollable() {
             const activeLink = document.querySelector(".top-nav-link.active");
-            const container = document.querySelector(".c-top-nav");
           
-            if (activeLink && container && container.scrollWidth > container.clientWidth) {
+            if (activeLink && topNav && topNav.scrollWidth > topNav.clientWidth) {
               // Proceed only if the container is scrollable
               const linkRect = activeLink.getBoundingClientRect();
-              const containerRect = container.getBoundingClientRect();
+              const containerRect = topNav.getBoundingClientRect();
           
               // Calculate the horizontal scroll offset
               const offset = (containerRect.width / 2) - (linkRect.width / 2);
               const scrollPosition = activeLink.offsetLeft - offset;
           
               // Smoothly scroll the container to center the active link
-              container.scrollTo({
+              topNav.scrollTo({
                 left: scrollPosition
               });
             }
@@ -41,7 +40,11 @@ import 'modules/share-form';
         // Function to update the active link
         const updateActiveLink = () => {
             let topSection = null;
-            const headerBottom = header.getBoundingClientRect().bottom;
+            let navBottom = 0;
+
+            if (window.getComputedStyle(topNav).display !== "none") {
+                navBottom = topNav.getBoundingClientRect().bottom;
+            }
         
             // Find the topmost section that is visible
             sections.forEach((section) => {
@@ -49,10 +52,10 @@ import 'modules/share-form';
 
                 // section is visible: 
                 //      it is not above the viewport && 
-                //      it is not above the bottom of the header (which matters if the header is sticky) && 
+                //      it is not above the bottom of the top nav (which matters if the top nav is sticky) && 
                 //      it is not below the viewport
                 if ( rect.bottom >= (0 + WINDOW_DELTA) && 
-                     rect.bottom >= (headerBottom + WINDOW_DELTA) && 
+                     rect.bottom >= (navBottom + WINDOW_DELTA) && 
                      rect.top < (window.innerHeight - WINDOW_DELTA) ) {
                     if (!topSection || rect.top < topSection.getBoundingClientRect().top) {
                         topSection = section;
@@ -80,7 +83,7 @@ import 'modules/share-form';
                 }
 
                 // Add the hash to the URL unless the user is at the top of the page
-                if (window.scrollY > headerBottom) {
+                if (window.scrollY > navBottom) {
                     history.pushState(null, "", `#${topSection.id}`);
                 }
             }
@@ -96,32 +99,46 @@ import 'modules/share-form';
 
         // Adjust scroll for direct hash navigation on page load
         if (window.location.hash) {
-            const headerBottom = header.getBoundingClientRect().bottom;
-            const headerSize = header.getBoundingClientRect().bottom - header.getBoundingClientRect().top;
-            if (headerBottom > 0) {
-                const targetId = window.location.hash.substring(1);
-                const targetElement = document.getElementById(targetId);
-                if (targetElement) {
-                    const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - headerSize;
-                    window.scrollTo({
-                        top: targetPosition
-                    });
+            requestAnimationFrame(() => {
+                let navBottom = topNav.getBoundingClientRect().bottom;
+                let navSize = navBottom - topNav.getBoundingClientRect().top;
+    
+                if (window.getComputedStyle(topNav).display !== "none") {
+                    navBottom = topNav.getBoundingClientRect().bottom;
+                    navSize = navBottom - topNav.getBoundingClientRect().top;
                 }
-            }
+    
+                if (navBottom > 0) {
+                    const targetId = window.location.hash.substring(1);
+                    const targetElement = document.getElementById(targetId);
+                    if (targetElement) {
+                        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - navSize;
+                        window.scrollTo({
+                            top: targetPosition
+                        });
+                    }
+                }
+            })
         }
 
         // Adjust scroll when clicking jump links
         document.querySelectorAll(".top-nav-link").forEach((link) => {
             link.addEventListener("click", (e) => {
-                const headerBottom = header.getBoundingClientRect().bottom;
-                const headerSize = header.getBoundingClientRect().bottom - header.getBoundingClientRect().top;
-                if (headerBottom > 0) {
+                let navBottom = 0;
+                let navSize = 0;
+
+                if (window.getComputedStyle(topNav).display !== "none") {
+                    navBottom = topNav.getBoundingClientRect().bottom;
+                    navSize = navBottom - topNav.getBoundingClientRect().top;
+                }
+
+                if (navBottom > 0) {
                     e.preventDefault(); // Prevent default jump behavior
                     const targetId = link.getAttribute("href").substring(1);
                     const targetElement = document.getElementById(targetId);
 
                     if (targetElement) {
-                        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - headerSize;
+                        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - navSize;
                         window.scrollTo({
                             top: targetPosition
                         });
