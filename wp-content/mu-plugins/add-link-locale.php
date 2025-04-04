b1c0f751246c64e5e3613dc3181229d96742f8fd
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2025-04-04 14:22:26 -0400

    use rtrim and add doc comment

--- wp-content/mu-plugins/add-link-locale.php
+++ wp-content/mu-plugins/add-link-locale.php
@@ -1,19 +1,25 @@
 <?php
 
-// Add locale to a link if it currently doesn't exist
-// Examples, if the locale is 'es':
-// https://jobs.nyc.gov/employers -> https://jobs.nyc.gov/es/employers
-// https://jobs.nyc.gov/es/employers -> https://jobs.nyc.gov/es/employers
-// If locale is 'en':
-// https://jobs.nyc.gov/employers -> https://jobs.nyc.gov/employers
+/**
+ * Add locale to a link if it currently doesn't exist
+ * Examples, if the locale is 'es':
+ * https://access.nyc.gov/programs -> https://access.nyc.gov/es/programs
+ * https://access.nyc.gov/es/programs -> https://access.nyc.gov/es/programs
+ * If locale is 'en':
+ * https://access.nyc.gov/programs -> https://access.nyc.gov/programs
+ * 
+ * @param string $original_url    The original URL that may need localization
+ * @param string $site_url        The base site URL without locale
+ * @param string $site_url_localized The site URL with locale segment
+ * @return string                 The URL with proper localization
+ */
 if (! function_exists('convert_link_locale')) {
   function convert_link_locale($original_url, $site_url, $site_url_localized) {
+    $site_url = rtrim($site_url, '/');
+    $site_url_localized = rtrim($site_url_localized, '/');
+
     if (strpos($original_url, $site_url_localized) === false) {
-      $site_url_localized_to_use = $site_url_localized;
-      if (substr($site_url_localized_to_use, -1) === "/") {
-        $site_url_localized_to_use = substr_replace($site_url_localized_to_use, "", -1);
-      }
-      return str_replace($site_url, $site_url_localized_to_use, $original_url);
+      return str_replace($site_url, $site_url_localized, $original_url);
     }
     // if original url is already localized, return it
     return $original_url;
