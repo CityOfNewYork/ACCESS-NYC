044c9c06538e43b3e367c8624e4acc56784339c3
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2025-04-25 11:30:46 -0400

    remove screening results from sitemap

--- wp-content/mu-plugins/core-sitemaps.php
+++ wp-content/mu-plugins/core-sitemaps.php
@@ -41,3 +41,15 @@ add_filter('wp_sitemaps_post_types', function($post_types) {
 
   return $post_types;
 });
+
+// Remove eligibility results from sitemap
+add_filter( 'wp_sitemaps_posts_query_args', function( $args, $post_type ) {
+  if ( $post_type === 'page' ) {
+      // Get the ID of the page you want to exclude
+      $page_to_exclude = get_page_by_path( 'eligibility/results' );
+      if ( $page_to_exclude ) {
+          $args['post__not_in'][] = $page_to_exclude->ID;
+      }
+  }
+  return $args;
+}, 10, 2 );
