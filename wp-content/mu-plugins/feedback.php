67257595654aeefd9178617170e808a56a5c3d8a
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2025-07-27 22:09:02 -0400

    connected to new Airtable API

--- wp-content/mu-plugins/feedback.php
+++ wp-content/mu-plugins/feedback.php
@@ -2,10 +2,8 @@
 
 namespace FeedbackNYC;
 
-use \TANIOS\Airtable\Airtable;
-
-// add_action('wp_ajax_feedback', 'FeedbackNYC\feedbackHandler');
-// add_action('wp_ajax_nopriv_feedback', 'FeedbackNYC\feedbackHandler');
+add_action('wp_ajax_feedback', 'FeedbackNYC\feedbackHandler');
+add_action('wp_ajax_nopriv_feedback', 'FeedbackNYC\feedbackHandler');
 
 /**
  * Creates a record on an Airtable based on the feedback form submission.
@@ -13,20 +11,46 @@ use \TANIOS\Airtable\Airtable;
  * @return Array - If successful, returns a response from Airtable client.
  */
 function feedbackHandler() {
+  $AIRTABLE_API_URL = 'https://api.airtable.com/v0';
+
   $nonce = $_POST['feedback-nonce'];
 
   if (wp_verify_nonce($nonce, 'feedback')) {
     try {
-      $client = get_airtable_client();
-      $feedback_fields = get_values_from_submission($_POST);
-      $airtable_record = create_record($feedback_fields, $client);
+      if (defined('AIRTABLE_FEEDBACK_API_TOKEN') && defined('AIRTABLE_FEEDBACK_BASE_KEY') && defined('AIRTABLE_FEEDBACK_TABLE_KEY')) {
+        $feedback_fields = get_values_from_submission($_POST);
+        $api_request_url = $AIRTABLE_API_URL . '/' . AIRTABLE_FEEDBACK_BASE_KEY . '/' . AIRTABLE_FEEDBACK_TABLE_KEY;
 
-      wp_send_json([
-        'success' => true,
-        'error' => 200,
-        'message' => __('Thank you for your feedback.'),
-        'retry' => false
-      ]);
+        error_log($api_request_url);
+        error_log(print_r($feedback_fields, true));
+
+        $response = wp_remote_post($api_request_url, [
+          'headers' => [
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . AIRTABLE_FEEDBACK_API_TOKEN
+          ],
+          'body' => wp_json_encode($feedback_fields),
+          'method' => 'POST',
+          'data_format' => 'body',
+        ]);
+  
+        if (is_wp_error($response)) {
+          error_log('WP error when submitting feedback to Airtable: ' . $response->get_error_message());
+          failure(400, 'Error when submitting feedback');
+        } elseif (wp_remote_retrieve_response_code($response) !== 200) {
+          error_log('Bad response submitting feedback to Airtable: ' . wp_remote_retrieve_body($response));
+          failure(400, 'Error when submitting feedback');
+        }
+        else {
+          wp_send_json([
+            'success' => true,
+            'error' => 200,
+            'message' => __('Thank you for your feedback.'),
+            'retry' => false
+          ]);
+        }
+      }
+      
     } catch (Exception $e) {
       $message = $e->getMessage();
 
@@ -57,27 +81,6 @@ function get_airtable_client() {
 }
 
 /**
- * Creates a record in an Airtable table.
- *
- * @param Array $args - The form fields and values from the submission.
- * @param Array $client - The Airtable PHP client.
- *
- * @return Array Response from Airtable regarding creation of a table record.
- */
-function create_record($args, $client) {
-  $new_record = $client->saveContent(AIRTABLE_FEEDBACK_TABLE_NAME, $args);
-  $client_response = (array) $new_record;
-
-  foreach ($client_response as $key => $value) {
-    if (isset($value->error)) {
-      failure(400, "{$value->error->message}");
-    }
-  }
-
-  return $new_record;
-}
-
-/**
  * Pass values from the submission inthe Airtable fields
  *
  * @param Array $submission - The POST request data from the feedback form.
@@ -86,10 +89,12 @@ function create_record($args, $client) {
  */
 function get_values_from_submission($submission) {
   $feedback_fields = array(
-    'helpful'        => $submission['helpful'],
-    'description'    => $submission['description'],
-    'program'        => $submission['program'],
-    'browser'        => $_SERVER['HTTP_USER_AGENT']
+    'fields' => array(
+      'helpful'        => $submission['helpful'],
+      'description'    => $submission['description'],
+      'program'        => $submission['program'],
+      'browser'        => $_SERVER['HTTP_USER_AGENT']
+    )
   );
 
   return $feedback_fields;
