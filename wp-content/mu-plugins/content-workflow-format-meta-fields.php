3c4d22145e58327117363aef0f7b7ce892211762
Sourabh Chakraborty <schakraborty@nycopportunity.nyc.gov>
2025-08-11 16:18:54 -0400

    gettype

--- wp-content/mu-plugins/content-workflow-format-meta-fields.php
+++ wp-content/mu-plugins/content-workflow-format-meta-fields.php
@@ -10,4 +10,5 @@ add_filter('cwby_config_pre_meta_field_value_updated', function($unused_param, $
   error_log("$meta_value: " . print_r($meta_value, true));
   error_log("$meta_key: " . print_r($meta_key, true));
   error_log("$push_object: " . print_r($push_object, true));
+  error_log("$meta_value type: " . print_r(gettype($meta_value), true));
 });
