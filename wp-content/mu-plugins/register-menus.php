21adb3ba6f0c2ff8939d20a6264b2ac86e6347da
Joy Lan <joylan823@gmail.com>
2026-02-26 09:11:29 -0500

    Remove unused footer section

--- wp-content/mu-plugins/register-menus.php
+++ wp-content/mu-plugins/register-menus.php
@@ -11,7 +11,6 @@ add_action('init', function() {
       'header-menu' => __('Nav bar'),
       'header-menu-b' => __('Nav bar [variant B for A/B testing]'),
       'get-help-now' => __('Footer Menu 1 [left]'),
-      'for-caseworkers' => __('Footer Menu 2 [left]'),
       'programs' => __('Footer Menu 3 [center]'),
       'about-access-nyc' => __('Footer Menu 4 [right]'),
     )
